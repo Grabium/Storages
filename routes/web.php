af64bc89ed6dbcb140abf8cde30c0184e8b17a84
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2024-05-03 19:10:25 +0000

    #2 - endpoint:
    https://92494a40-e76a-4c30-a7ad-bbe6a97c3df2-00-35tw3ddlxlec3.worf.replit.dev/

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//https://92494a40-e76a-4c30-a7ad-bbe6a97c3df2-00-35tw3ddlxlec3.worf.replit.dev/
 
 Route::get('/', [App\Http\Controllers\Storages\RecuperarArquivosController::class, 'testarExistenciaDeArquivo']);
 
